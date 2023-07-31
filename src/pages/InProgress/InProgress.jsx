fa8e98164bc002121c17c5b673de029a41f11137
dspetrovv <dimon4540@gmail.com>
2023-07-31 16:35:20 +0300

    feature: added tests and dockerfile

--- src/pages/InProgress/InProgress.jsx
+++ src/pages/InProgress/InProgress.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './in-progress.module.scss';
 
-const title = 'Упс... Страница ещё не готова, но спешим из всех сил ;)';
+const title = 'Упс... Страница ещё не готова, но спешим изо всех сил ;)';
 
 const InProgress = () => {
   // Можно в теории добавить красивую картинку, но это потом
